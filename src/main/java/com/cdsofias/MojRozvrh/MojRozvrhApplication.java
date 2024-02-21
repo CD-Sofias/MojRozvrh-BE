0b09f20413571f5ce91f919f7be442d57534bf7d
sscorp1on <german4941@gmail.com>
2024-02-21 17:29:59 +0100

    initial spring boot commit

---
+++ src/main/java/com/cdsofias/MojRozvrh/MojRozvrhApplication.java
@@ -0,0 +1,13 @@
+package com.cdsofias.MojRozvrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojRozvrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojRozvrhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdsofias/MojRozvrh/MojRozvrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdsofias.MojRozvrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojRozvrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
