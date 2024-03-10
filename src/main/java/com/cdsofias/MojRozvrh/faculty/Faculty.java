0723ae4917e4c7c05c6fe3fbcd4b295b194dbcf0
DmitryByrigin <dmitri.d907@gmail.com>
2024-03-10 16:09:18 +0100

    Added department and faculty modules

---
+++ src/main/java/com/cdsofias/MojRozvrh/faculty/Faculty.java
@@ -0,0 +1,24 @@
+package com.cdsofias.MojRozvrh.faculty;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "faculty")
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Faculty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+}
