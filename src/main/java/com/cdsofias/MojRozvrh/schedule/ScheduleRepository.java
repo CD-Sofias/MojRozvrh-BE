e3718854b8803ffb3b794b8b414ac612bda1fb5e
Aspergillusplay <kartofka123@i.ua>
2024-03-10 15:56:53 +0200

    Fixes imports

--- src/main/java/com/cdsofias/MojRozvrh/schedule/ScheduleRepository.java
+++ src/main/java/com/cdsofias/MojRozvrh/schedule/ScheduleRepository.java
@@ -2,7 +2,6 @@ package com.cdsofias.MojRozvrh.schedule;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
--- src/main/java/com/cdsofias/MojRozvrh/subjects/SubjectRepository.java
+++ src/main/java/com/cdsofias/MojRozvrh/subjects/SubjectRepository.java
@@ -1,7 +1,6 @@
 package com.cdsofias.MojRozvrh.subjects;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
