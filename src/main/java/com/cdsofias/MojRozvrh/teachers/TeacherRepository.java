dfa1f301e3d6668fa84e2edc198576f4c6f4471a
Aspergillusplay <kartofka123@i.ua>
2024-03-10 16:17:35 +0200

    Fixes in teacher repository

--- src/main/java/com/cdsofias/MojRozvrh/teachers/TeacherRepository.java
+++ src/main/java/com/cdsofias/MojRozvrh/teachers/TeacherRepository.java
@@ -1,10 +1,10 @@
 package com.cdsofias.MojRozvrh.teachers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.UUID;
 
-@Repository
+
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
 }
